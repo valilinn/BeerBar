ce73bfb870f81bce2f104246c7fedd3177932902
valilinn <valentinalinchuk@gmail.com>
2023-06-24 12:12:53 +0200

    Added info with links

--- BeerBar/BeerBar/ModalViewController.swift
+++ BeerBar/BeerBar/ModalViewController.swift
@@ -6,8 +6,10 @@
 //
 
 import UIKit
+import MessageUI
 
-class ModalViewController: UIViewController {
+
+class ModalViewController: UIViewController, MFMailComposeViewControllerDelegate {
     
     
     
@@ -15,13 +17,39 @@ class ModalViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
     }
     
 
+    @IBAction func email(_ sender: Any) {
+      
+        if let url = URL(string: "https://t.me/valinchuk") {
+            UIApplication.shared.open(url)
+        }
+        
+    }
     
     
+    @IBAction func reallyEmail(_ sender: Any) {
+        if MFMailComposeViewController.canSendMail() {
+                    let mail = MFMailComposeViewController()
+                    mail.mailComposeDelegate = self
+                    mail.setToRecipients(["valentinalinchuk@gmail.com"])
+                    mail.setSubject("Subject")
+                    mail.setMessageBody("<p>You're so awesome!</p>", isHTML: true)
+//                    mail.setCcRecipients(["valentinalinchuk@gmail.com"])
+                    
+                    present(mail, animated: true)
+                } else {
+                    // show failure alert
+                }
+    }
     
-    
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+            //... handle email screen actions
+            controller.dismiss(animated: true)
+        }
 
 }
